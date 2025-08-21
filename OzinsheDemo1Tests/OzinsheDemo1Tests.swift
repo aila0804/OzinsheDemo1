cba1324c8f6942c332c644f869a28a13830c5644
aila0804 <aila_d@mail.ru>
2025-08-21 23:25:21 +0500

    first commit

---
+++ OzinsheDemo1Tests/OzinsheDemo1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  OzinsheDemo1Tests.swift
+//  OzinsheDemo1Tests
+//
+//  Created by Aila Aila on 21.08.2025.
+//
+
+import Testing
+@testable import OzinsheDemo1
+
+struct OzinsheDemo1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
