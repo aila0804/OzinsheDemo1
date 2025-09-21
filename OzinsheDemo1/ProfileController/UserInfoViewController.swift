c7cb3f400fd107bb34f1da8dbe62f8b40dc3ed59
aila0804 <aila_d@mail.ru>
2025-09-21 19:01:37 +0500

    updated

--- OzinsheDemo1/ProfileController/UserInfoViewController.swift
+++ OzinsheDemo1/ProfileController/UserInfoViewController.swift
@@ -90,5 +90,53 @@ class UserInfoViewController: UIViewController {
         return view
     }()
     
+    let birthLabel = {
+        let label = UILabel()
+        
+        label.text = "Туылған күні"
+        label.textColor = UIColor(red: 0.612, green: 0.639, blue: 0.686, alpha: 1)
+        label.font = UIFont(name: "SF-Pro-Display-Bold", size: 14)
+        
+        return label
+    }()
+    
+    let birthTextField = {
+        let textField = UITextField()
+        
+        textField.placeholder = "2005-01-28"
+        textField.textColor = UIColor(named: "111827")
+        textField.font = UIFont (name: "SF-Pro-Display-Semibold", size: 16)
+        
+        return textField
+    }()
+    
+    let grayView4 = {
+        let view = UIView()
+        view.backgroundColor = UIColor(red: 0.82, green: 0.835, blue: 0.859, alpha: 1)
+        
+        return view
+    }()
+    
+    lazy var saveChangesButton = {
+        let button = UIButton()
+        
+        button.setTitle("Өзгерістерді caқтay", for: .normal)
+        button.backgroundColor = UIColor(red: 0.5, green: 0.18, blue: 0.99, alpha: 1)
+        button.titleLabel?.font = UIFont (name: "SF-Pro-Display-Semibold", size: 16)
+        button.layer.cornerRadius = 12
+        button.addTarget(self, action: #selector(saveInfoButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    // cycle
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+//    setupUI()
+//    downloadPersonalInfo()
+//    localizeLanguage()
+        
+    }
     
 }
