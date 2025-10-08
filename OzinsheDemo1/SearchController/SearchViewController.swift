c755cac0637d10100781988772494d8a5530fe58
aila0804 <aila_d@mail.ru>
2025-10-08 23:55:17 +0500

    updated

--- OzinsheDemo1/SearchController/SearchViewController.swift
+++ OzinsheDemo1/SearchController/SearchViewController.swift
@@ -255,6 +255,64 @@ class SearchViewController: UIViewController {
     }
     SVProgressHUD.show()
     
+    let
+    headers: HTTPHeaders = [ "Authorization": "Bearer\(Storage.sharedInstance.accessToken)"]
+    let parametres = ["search": searchTextField.text!]
+    AF.request(Urls.SEARCH_MOVIES_URL, method: .get, parameters: parametres, headers: headers).responseData { response in
+        
+        SVProgressHUD.dismiss()
+        
+        var if resultString =
+                let data = response.data {
+            resultString = String(data: data, encoding: .utf8)!
+            print(resultString)
+        }
+        
+        if response.response?.statusCode == 200 {
+            let json = JSON(response.data!)
+            print("JSON: \(json)")
+            
+            if let array = json.array {
+                self.movies.removeAll()
+                self.tableView.reloadData()
+                for item in array{
+                    let movie = Movie(json: item)
+                    self movies.append(movie)
+                }
+                self.tableView.reloadData ()
+            } else {
+                SVProgressHUD.showError(withStatus: "CONNECTION_ERROR".localized())
+            }
+        } else {
+            var
+            ErrorStrIng = "CONNECTION_ERROR".localized()
+            if let sCode = response.response?.statusCode {
+                ErrorString = ErrorString + "\(sCode)"
+            }
+            ErrorString = ErrorString + "\(resultString) "
+            SVProgressHUD.showError(withStatus: "\(ErrorString)")
+        }
+    }
+}
+
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return movies.count
+}
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableCell", for: indexPath) as! MovieTableViewCell
+        cell.setData(movie: movies[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 153.0
+    }
+    
     //    override func viewDidLoad() {
     //        super.viewDidLoad()
     //        view.backgroundColor = .white
