bf141152ccfbfadc297d2eaf98296251877c8969
aila0804 <aila_d@mail.ru>
2025-11-06 02:11:59 +0500

    updated

--- OzinsheDemo1/MainPageViewController/HistoryTableViewCell.swift
+++ OzinsheDemo1/MainPageViewController/HistoryTableViewCell.swift
@@ -38,11 +38,11 @@ class HistoryTableViewCell: UITableViewCell {
         let label = UILabel()
         label.text = "Карауды жалгастырыныз"
         label.font = UIFont(name: "SFProDisplay-Bold", size: 16)
-        label.textColor = UIColor(named: "111827 - FFFFFF")
+        label.textColor = UIColor(named: "111827")
         return label
     }()
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         historyCollection.dataSource = self
@@ -55,4 +55,62 @@ class HistoryTableViewCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    func setData (mainMovie: MainMovies) {
+        
+        self.mainMovie = mainMovie
+        
+        historyCollection.reloadData ()
+    }
+    
+        func setupUI() {
+            
+            contentView.backgroundColor = UIColor(named: "111827" )
+            
+            contentView.addSubviews (historyCollection, titleLabel)
+            
+            historyCollection.snp.makeConstraints { make in
+                make.top.equalTo(contentView.safeAreaLayoutGuide)
+                make.right.equalToSuperview()
+                make.left.equalToSuperview()
+                make.bottom.equalToSuperview()
+            }
+            
+            titleLabel.snp.makeConstraints { make in
+                make.top.equalToSuperview().inset(22)
+                make.left.equalToSuperview().inset(24)
+                make.right.equalToSuperview().inset(24)
+                make.height.equalTo(19)
+            }
+        }
+    }
+    
+extension HistoryTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mainMovie.movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HistoryCollectionCell", for: indexPath) as! HistoryCollectionViewCell
+        
+        let transformer = SDImageResizingTransformer(size: CGSize(width: 184, height: 112), scaleMode: .aspectFill)
+        
+        cell.image.sd_setImage(with: URL(string: mainMovie.movies[indexPath.row].poster_link), placeholderImage: nil, context: [.imageTransformer: transformer])
+        cell.image.layer.cornerRadius = 8
+        cell.titleLabel.text = mainMovie.movies[indexPath.row].name
+        
+        if let genrename = mainMovie.movies[indexPath.row].genres.first {
+            cell.subtitleLabel.text = genrename.name
+        } else {
+            cell.subtitleLabel.text = ""
+        }
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        delegate?.movieDidSelect (movie: mainMovie.movies[indexPath.row])
+    }
 }
