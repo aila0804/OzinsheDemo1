d7ed5613efcb8535db197e5ede4171446f94b2bb
aila0804 <aila_d@mail.ru>
2025-11-09 23:57:37 +0500

    updated

--- OzinsheDemo1/MainPageViewController/MainCollectionViewCell.swift
+++ OzinsheDemo1/MainPageViewController/MainCollectionViewCell.swift
@@ -31,4 +31,47 @@ class MainCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
+    let subtitleLabel = {
+        let label = UILabel()
+        label.text = "Мультсериал"
+        label.textColor = UIColor(red: 0.61, green: 0.64, blue: 0.69, alpha: 1)
+        label.font = UIFont (name: "SF-Pro-Display-Regular", size: 12)
+        
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init (frame: frame)
+        
+        setupUI()
+        backgroundColor = UIColor (named: "111827")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        
+        contentView.addSubviews(image, titleLabel, subtitleLabel)
+        
+        image.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.right.equalToSuperview()
+            make.left.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalTo(image.snp.bottom).offset(8)
+        }
+        
+        subtitleLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.bottom.equalToSuperview()
+        }
+    }
 }
