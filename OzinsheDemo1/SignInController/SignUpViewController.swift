fd0b3621d7621885031cead13419be445d696824
aila0804 <aila_d@mail.ru>
2025-11-17 23:20:04 +0500

    updated

--- OzinsheDemo1/SignInController/SignUpViewController.swift
+++ OzinsheDemo1/SignInController/SignUpViewController.swift
@@ -69,5 +69,100 @@ class SignUpViewController: UIViewController {
         return label
     }()
     
+    let passwordTextField = {
+        let tf = TextFieldWithPadding()
+        tf.placeholder = "Сіздін купия сезініз"
+        tf.font = UIFont (name: "SF-Pro-Display-Regular", size: 16)
+        tf.textColor = UIColor(named: "111827")
+        tf.isSecureTextEntry = true
+        tf.layer.cornerRadius = 12
+        tf.layer.borderWidth = 1
+        
+        return tf
+    }()
+    
+    let passwordImage = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "password")
+        
+        return iv
+    }()
+    
+    lazy var showPasswordButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "showPassword"), for: .normal)
+        button.addTarget(self, action: #selector(showPassTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    let repeatPasswordLabel = {
+        let label = UILabel()
+        label.text = "Купиясоз"
+        label. font = UIFont (name: "SF-Pro-Display-Bold", size: 14)
+        label.textColor = UIColor(named: "111827")
+        
+        return label
+    }()
+    
+    let repeatPasswordTextField = {
+        let tf = TextFieldWithPadding()
+        tf.placeholder = "Сіздін купия сезініз"
+        tf.font = UIFont(name: "SF-Pro-Display-Regular", size: 16)
+        tf.textColor = UIColor(named: "111827")
+        tf.layer.borderColor = UIColor(red: 0.90, green: 0.92, blue: 0.94, alpha: 1.00).cgColor
+        tf.isSecureTextEntry = true
+        tf.layer.cornerRadius = 12
+        tf.layer.borderWidth = 1
+        
+        return tf
+    }()
+    
+    let repeatPasswordImage = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "password")
+        
+        return iv
+    }()
+    
+    lazy var repeatShowPasswordButton = {
+        let button = UIButton()
+        button. setImage (UIImage (named: "showPassword"), for: .normal)
+        button.addTarget(self, action: #selector (repeatShowPassTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var signUpButton = {
+        let button = UIButton()
+        button.setTitle("Тіркелу", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont (name: "SF-Pro-Display-Semibold", size: 16)
+        button. backgroundColor = UIColor(red: 0.49, green: 0.18, blue: 0.99, alpha: 1.00)
+        button.layer.cornerRadius = 12
+        button.addTarget (self, action: #selector(signUpTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    lazy var signInButton = {
+        let button = UIButton()
+        button.setTitle("Kipy", for: .normal)
+        button.titleLabel?. font = UIFont (name: "SF-Pro-Display-Semibold", size: 14)
+        button.setTitleColor(UIColor(red: 0.49, green: 0.18, blue: 0.99, alpha: 1.00), for: .normal)
+        button. addTarget(self, action: #selector(signInTapped), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    let questionLabel = {
+        let label = UILabel()
+        label.text = "Сізде аккаунт бар ма?"
+        label.font = UIFont (name: "SF-Pro-Display-Semibold", size: 14)
+        label.textColor = UIColor(red: 0.42, green: 0.45, blue: 0.50, alpha: 1.00)
+        label.textAlignment = .right
+        
+        return label
+    }()
         
 }
