467478ef96aaf77bf0a9adb344ab1214d675f246
aila0804 <aila_d@mail.ru>
2025-11-26 23:56:31 +0500

    updated

--- OzinsheDemo1/SearchController/CategoryTableViewController.swift
+++ OzinsheDemo1/SearchController/CategoryTableViewController.swift
@@ -45,23 +45,79 @@ class CategoryTableViewController: UITableViewController {
     }
     
     func downloadMoviesByCategory) {
-    SVProgressHUD.show()
-    let headers: HTTPHeaders = ["Authorization": "Bearer\(Storage.sharedInstance.accessToken)"]
-    let parametres = ["categoryId": categoryID]
-    self.isLoading = false
-    self.refreshControl?.endRefreshing()
-    AF.request(Urls.MOVIES_BY_CATEGORY_URL, method: parametres, headers: headers) responseData { response in
-    • get, parameters:
-
-    SVProgressHUD.dismiss ( )
-    var if
-    resultString =
-    let data
-    =
-    response.data {
-    resultString = String(data: data,
-    print (resultString)
-    encoding:
-    •utf8)!
+        SVProgressHUD.show()
+        
+        let headers: HTTPHeaders = ["Authorization": "Bearer\(Storage.sharedInstance.accessToken)"]
+        
+        let parametres = ["categoryId": categoryID]
+        
+        self.isLoading = false
+        self.refreshControl?.endRefreshing()
+        
+        AF.request(Urls.MOVIES_BY_CATEGORY_URL, method: .get, parameters: parametres, headers: headers).responseData { response in
+            
+            SVProgressHUD.dismiss()
+            var if resultString = ""
+                    let data = response.data {
+                resultString = String(data: data, encoding: .utf8)!
+                print(resultString)
+            }
+            
+            if response.response?.statusCode ＝= 200 {
+                let json = JSON(response.data!)
+                print("JSON: \(json)")
+                
+                if json["content"].exists() {
+                    if let array = json["content"].array {
+                        for item in array {
+                            let movie = Movie(json: item)
+                            self movies.append(movie)
+                        }
+                        self.tableView.reloadData()
+                    } else {
+                        SVProgressHUD.showError(withStatus: "CONNECTION_ERROR".localized())
+                    }
+                } else {
+                    var ErrorString = "CONNECTION_ERROR".localized()
+                    if let sCode = response.response?.statusCode {
+                        ErrorString = ErrorString + "\ (sCode)"
+                    }
+                    ErrorString = ErrorString + "\(resultString)"
+                    SVProgressHUD.showError(withStatus: "\(ErrorString)")
+                }
+            }
+        }
+    }
     
+    // Table view data source
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return movies.count
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieTableCell", for: indexPath) as! MovieTableViewCell
+        
+        cell.setData(movie: movies[indexPath.row])
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 153
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let movieinfoVC = MovieInfoController()
+        
+        movieinfoVC.movie = movies[indexPath.row]
+        
+        navigationController?.show(movieinfoVC, sender: self)
+    }
+}
